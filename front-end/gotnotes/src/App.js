199efb97c4c49b2af2a5a5d2dcc979ed1d35dbdd
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-10-25 01:38:51 -0400

    added some comments

--- front-end/gotnotes/src/App.js
+++ front-end/gotnotes/src/App.js
@@ -1,10 +1,19 @@
 import { ListItem } from './components/Mobile/ListItem';
 import './default.scss';
 
-import {mockUniData} from './assets/mocks/mockData'
+import {
+  mockUniData, 
+  mockClassData, 
+  mockFileData, 
+  mockUserData
+} from './assets/mocks/mockData'
 
 
 function App() {
+  //ANCHOR: for now we are using the mock data imported above to test our designs
+  //let's focus on overall functionality of individual components, 
+  //before we tie everything together and code up the app logic
+  
   return (
     <div className="App"> 
     {mockUniData.map(({itemID, itemName, itemLogoPath, itemType}) => (
