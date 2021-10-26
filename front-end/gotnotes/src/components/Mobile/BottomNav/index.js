52a16ca779c534bb0149bb721aefdebe82b1e020
jap871 <jap871@nyu.edu>
2021-10-26 13:24:31 -0400

    bottomNav - trying to work

--- front-end/gotnotes/src/components/Mobile/BottomNav/index.js
+++ front-end/gotnotes/src/components/Mobile/BottomNav/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-<<<<<<< HEAD
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-=======
 
 import './styles.scss'
 
 //icons
->>>>>>> eef66d91805a20058dd9530c1bdeb0106cf61f25
 import SearchIcon from '@mui/icons-material/Search';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
@@ -16,23 +12,6 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 //comment
 
 const tabs = [{
-<<<<<<< HEAD
-    route: "/search/unis",
-    icon: SearchIcon,
-    label: "Search"
-},{
-    route: "/chat",
-    icon: ChatIcon,
-    label: "Chat"
-},{
-    route: "/addFile",
-    icon: AddIcon,
-    label: "AddFile"
-},{
-    route: "/account",
-    icon: PersonIcon,
-    label: "Account"
-=======
   route: "/unis",
   icon: <SearchIcon fontSize="large"/>,
   label: "Unis"
@@ -48,7 +27,6 @@ const tabs = [{
   route: "/account",
   icon: <PersonOutlineOutlinedIcon fontSize="large"/>,
   label: "Account"
->>>>>>> eef66d91805a20058dd9530c1bdeb0106cf61f25
 }]
 
 const BottomNav = (props) => {
@@ -73,27 +51,6 @@ const BottomNav = (props) => {
           </div>
         </Nav>
       </nav>
-<<<<<<< HEAD
-    <nav className="navbar fixed-bottom navbar-light d-block d-lg-none bottom-tab-nav" role="navigation">
-      <Nav className="w-100">
-        <div className=" d-flex flex-row justify-content-around w-100">
-          {
-            tabs.map((tab, index) =>(
-              <NavItem key={`tab-${index}`}>
-                <NavLink to={tab.route} className="nav-link bottom-nav-link" activeClassName="active">
-                  <div className="row d-flex flex-column justify-content-center align-items-center">
-                    {/*<FontAwesomeIcon size="lg" icon={tab.icon}/>*/}
-                    <div className="bottom-tab-label">{tab.label}</div>
-                  </div>
-                </NavLink>
-              </NavItem>
-            ))
-          }
-        </div>
-      </Nav>
-    </nav>
-=======
->>>>>>> eef66d91805a20058dd9530c1bdeb0106cf61f25
     </div>
   )
 };
