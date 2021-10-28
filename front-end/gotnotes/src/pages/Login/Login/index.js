8b2edfbd63662291096bc8ad5d8cacf76e59acbb
m4rker11 <79331578+m4rker11@users.noreply.github.com>
2021-10-27 21:58:24 -0400

    Login page no routing

--- front-end/gotnotes/src/pages/Login/Login/index.js
+++ front-end/gotnotes/src/pages/Login/Login/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Form from '../../../components/Mobile/Forms'
+import Button from '../../../components/Mobile/Button'
+import logo from '../../../assets/GotNotes.jpg'
+import "./styles.scss"
+import { ResetPass } from '../ResetPass'
+import {Route,Switch, Router} from 'react-router-dom'
+
+const ForgotPassword = () =>{
+    // <Router>
+    // <Switch>
+    // <Route path="/resetpass" render={() => (
+    //     <ResetPass />
+    //   )} />
+    //   </Switch>
+    //   </Router>
+    console.log("forgot password button working")
+}
 
 export const Login = () => {
+    
     return (
-        <div>
-            
+        <div className = "whole">
+            <h2 className = "login">  Login</h2>
+            <img src = {logo} className="image"  />
+           <Form placeholder1="Email" placeholder2="Password" />
+           <Button text = "Forgot Password?" onClick = {ForgotPassword}/>
+
         </div>
     )
 }
