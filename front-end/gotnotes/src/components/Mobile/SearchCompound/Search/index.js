34c99f25c7d7c6b72f49eecefbda30af946a1c97
jap871 <jap871@nyu.edu>
2021-10-27 23:06:06 -0400

    search bar edits 2

--- front-end/gotnotes/src/components/Mobile/SearchCompound/Search/index.js
+++ front-end/gotnotes/src/components/Mobile/SearchCompound/Search/index.js
@@ -1,30 +1,14 @@
 import React, { useState } from "react";
 import SearchBar from "material-ui-search-bar";
-//import { MenuItem, Select, Typography } from "@material-ui/core/";
-//import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
 
 export default function App() {
   const [criteria, setCriteria] = useState("1");
   const [searchItem, setSearchItem] = useState("");
   console.log(criteria);
   return (
-    //<MuiThemeProvider>
+
       <div className="main-content">
-        {/*<Select
-          displayEmpty
-          onChange={e => setCriteria(e.target.value)}
-          defaultValue={criteria}
-        >
-          <MenuItem disabled value="">
-            <em>Search By</em>
-          </MenuItem>
-          <MenuItem value={1}>First Name</MenuItem>
-          <MenuItem value={2}>Last Name</MenuItem>
-          <MenuItem value={3}>Phone Number</MenuItem>
-          <MenuItem value={4}>Email</MenuItem>
-        </Select>
-        <br />
-        <br />*/}
         <SearchBar
           value={searchItem}
           onChange={value => {
@@ -37,6 +21,5 @@ export default function App() {
           }}
         />
       </div>
-    //</MuiThemeProvider>
   );
 }
