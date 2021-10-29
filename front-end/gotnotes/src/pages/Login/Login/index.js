aedaf8cce130290212011ad15cba8ccbeb9c5956
m4rker11 <79331578+m4rker11@users.noreply.github.com>
2021-10-29 19:34:49 -0400

    Login, signup, reset password done

--- front-end/gotnotes/src/pages/Login/Login/index.js
+++ front-end/gotnotes/src/pages/Login/Login/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import Form from '../../../components/Mobile/Forms'
+<<<<<<< Updated upstream
 export const Login = () => {
     return (
         <div>
            <Form placeholder1="Email" placeholder2="email"/>
         </div>
+=======
+
+import Button from '../../../components/Mobile/Button'
+import logo from '../../../assets/GotNotes.jpg'
+import "./styles.scss"
+import {NavLink, Redirect} from 'react-router-dom'
+
+
+
+export const Login = () => {
+
+        return (
+
+            
+            <div className = "whole">
+                <img src = {logo} className="image"  />
+                <h2 className = "login">  Login</h2>
+                <div>
+            <text enableBackground= "false"> Don't have an account? </text>
+            <text>
+            <a href = "/signup"><Button text = "Sign up"/> </a>
+            </text>
+            </div>
+            <div className = "reset">
+            <NavLink to="/resetpass" >
+            <Button text = "Reset Password"/> 
+            </NavLink>
+            </div>
+            <Form placeholder1="Email" placeholder2="Password" />
+            
+            
+            
+            </div>
+>>>>>>> Stashed changes
     )
 }
+
