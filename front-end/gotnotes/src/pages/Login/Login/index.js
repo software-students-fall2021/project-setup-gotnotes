975ae2fd343db14db25a04e4b53f260eb31ee9a4
m4rker11 <79331578+m4rker11@users.noreply.github.com>
2021-10-29 20:04:48 -0400

    I dislike github

--- front-end/gotnotes/src/pages/Login/Login/index.js
+++ front-end/gotnotes/src/pages/Login/Login/index.js
@@ -4,34 +4,6 @@ import Form from '../../../components/Mobile/Forms'
 import Button from '../../../components/Mobile/Button'
 import logo from '../../../assets/GotNotes.jpg'
 import "./styles.scss"
-<<<<<<< HEAD
-import { ResetPass } from '../ResetPass'
-import {Route,Switch, Router} from 'react-router-dom'
-
-const ForgotPassword = () =>{
-    // <Router>
-    // <Switch>
-    // <Route path="/resetpass" render={() => (
-    //     <ResetPass />
-    //   )} />
-    //   </Switch>
-    //   </Router>
-    console.log("forgot password button working")
-}
-
-export const Login = () => {
-    
-    return (
-
-        <div className = "whole">
-            <h2 className = "login">  Login</h2>
-            <img src = {logo} className="image"  />
-           <Form placeholder1="Email" placeholder2="Password" />
-           <Button text = "Forgot Password?" onClick = {ForgotPassword}/>
-
-
-        </div>
-=======
 import {NavLink, Redirect} from 'react-router-dom'
 
 
@@ -60,7 +32,6 @@ export const Login = () => {
             
             
             </div>
->>>>>>> Mark's_edits
     )
 }
 
