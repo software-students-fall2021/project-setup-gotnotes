f7641156d759b8d2f0d85e521253341585b50704
Matthew Apuya <m.apuya19@gmail.com>
2021-10-31 17:57:35 -0400

    material-ui button

--- front-end/gotnotes/src/App.js
+++ front-end/gotnotes/src/App.js
@@ -1,120 +1,97 @@
-import './default.scss';
+import "./default.scss";
 
 import { Switch, Route, Redirect } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import {
-  currentUserID,
-  mockUserData,
-} from './assets/mocks/mockData'
+import { currentUserID, mockUserData } from "./assets/mocks/mockData";
 
 //Components
-import AdminToolbar from './components/AdminToolbar'
-import BottomNav from './components/Mobile/BottomNav';
+import AdminToolbar from "./components/AdminToolbar";
+import BottomNav from "./components/Mobile/BottomNav";
 
 //Pages
-import { Unis } from './pages/Search/Unis';
-import { Courses } from './pages/Search/Courses';
-import { Files } from './pages/Search/Files';
+import { Unis } from "./pages/Search/Unis";
+import { Courses } from "./pages/Search/Courses";
+import { Files } from "./pages/Search/Files";
 import { FileDetails } from "./pages/Search/FileDetails";
-import ChatApp from './pages/Chat/ChatMessages'
-import { SignUp } from './pages/Login/SignUp';
-import { Login } from './pages/Login/Login'
-import { ResetPass } from './pages/Login/ResetPass';
+import ChatApp from "./pages/Chat/ChatMessages";
+import { SignUp } from "./pages/Login/SignUp";
+import { Login } from "./pages/Login/Login";
+import { ResetPass } from "./pages/Login/ResetPass";
 
-import { ChatSearch } from './pages/Chat/ChatSearch';
+import { ChatSearch } from "./pages/Chat/ChatSearch";
 
-import { AddFile } from './pages/AddFile';
+import { AddFile } from "./pages/AddFile";
 
-import { Admin } from './pages/Admin'
+import { Admin } from "./pages/Admin";
 
-import { Account } from './pages/Account/Account';
-import { MobileLayoutSelector } from './layouts/Mobile/MobileLayoutSelector';
-
-
+import { Account } from "./pages/Account/Account";
+import { MobileLayoutSelector } from "./layouts/Mobile/MobileLayoutSelector";
 
 function App() {
   //ANCHOR: for now we are using the mock data imported above to test our designs
-  //let's focus on overall functionality of individual components, 
+  //let's focus on overall functionality of individual components,
   //before we tie everything together and code up the app logic
 
-  const [crumbs, setCrumbs] = useState(['Unis', 'Uni', 'Class']); //"Uni" needs to be the university that they choose
-
+  const [crumbs, setCrumbs] = useState(["Unis", "Uni", "Class"]); //"Uni" needs to be the university that they choose
 
   return (
-
     <div className="App">
-
-      <AdminToolbar props={{ currentUser: mockUserData.filter(user => user.userID === currentUserID)[0] }} />
+      <AdminToolbar
+        props={{
+          currentUser: mockUserData.filter(
+            (user) => user.userID === currentUserID
+          )[0],
+        }}
+      />
       <Switch>
         <Redirect exact from="/" to="unis" />
 
-        <Route exact path="/unis" render={() => (
-          <MobileLayoutSelector Component={Unis} />
-
-        )} />
-        <Route exact path="/unis/:uniName" render={() => (
-          <MobileLayoutSelector Component={Courses} />
-        )} />
-        <Route exact path="/unis/:uniName/:courseName" render={() => (
-          <MobileLayoutSelector Component={Files} />
-        )} />
-        <Route exact path="/unis/:uniName/:courseName/:fileName" render={() => (
-          <FileDetails />
-        )} />
-
-        <Route path="/signup" render={() => (
-          <SignUp />
-        )}
-        />
-        <Route path="/chat" render={() => (
-          <ChatApp />
-        )}
-        />
-        <Route path="/login" render={() => (
-          <Login />
-        )}
+        <Route
+          exact
+          path="/unis"
+          render={() => <MobileLayoutSelector Component={Unis} />}
         />
 
-        <Route path="/resetpass" render={() => (
-          <ResetPass />
-        )}
+        <Route
+          exact
+          path="/unis/:uniName"
+          render={() => <MobileLayoutSelector Component={Courses} />}
         />
 
-        <Route path="/chat" render={() => (
-          <ChatSearch />
-        )}
+        <Route
+          exact
+          path="/unis/:uniName/:courseName"
+          render={() => <MobileLayoutSelector Component={Files} />}
+        />
+        
+        <Route
+          exact
+          path="/unis/:uniName/:courseName/:fileName"
+          render={() => <FileDetails />}
         />
 
-        <Route path="/addFile" render={() => (
-          <AddFile />
-        )}
-        />
+        <Route path="/signup" render={() => <SignUp />} />
+        <Route path="/chat" render={() => <ChatApp />} />
+        <Route path="/login" render={() => <Login />} />
 
-        <Route path="/account" render={() => (
-          <Account />
-        )}
-        />
+        <Route path="/resetpass" render={() => <ResetPass />} />
 
+        <Route path="/chat" render={() => <ChatSearch />} />
 
-        <Route path="/admin"
-          render={() => (
-            <Admin />
-          )}
-        />
+        <Route path="/addFile" render={() => <AddFile />} />
+
+        <Route path="/account" render={() => <Account />} />
+
+        <Route path="/admin" render={() => <Admin />} />
       </Switch>
 
       <BottomNav />
-      {
-        /*
+      {/*
         <Breadcrumbs/>
         <Search/>
         
-        */
-      }
-
-
-
+        */}
     </div>
   );
 }
--- front-end/gotnotes/src/layouts/Mobile/MobileLayoutSelector.js
+++ front-end/gotnotes/src/layouts/Mobile/MobileLayoutSelector.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 //HOC
-import { SearchHigherOrder } from './../../components/Mobile/SearchHigherOrder'
+import { SearchHigherOrder } from "./../../components/Mobile/SearchHigherOrder";
 
-import { ListItem } from '../../components/Mobile/ListItem';
-import { GridItem } from '../../components/Mobile/GridItem'
+import { ListItem } from "../../components/Mobile/ListItem";
+import { GridItem } from "../../components/Mobile/GridItem";
+
 export const MobileLayoutSelector = ({ Component }) => {
+  const [currentLayout, setCurrentLayout] = useState("list");
 
-    const [currentLayout, setCurrentLayout] = useState("list");
-
-    return (
-        <div className="layout-container">
-            <SearchHigherOrder props={{ currentLayout, setCurrentLayout }} />
-            <Component ViewComponent={currentLayout === "list" ? ListItem : GridItem}  />
-        </div>
-    )
-}
+  return (
+    <div className="layout-container">
+      <SearchHigherOrder props={{ currentLayout, setCurrentLayout }} />
+      <Component
+        ViewComponent={currentLayout === "list" ? ListItem : GridItem}
+      />
+    </div>
+  );
+};
