ff1c7517a6c81a2510849ce0061230bba120fbf4
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-01 16:35:47 -0400

    breadcrumb styles updated

--- front-end/gotnotes/src/components/Mobile/Breadcrumbs/index.js
+++ front-end/gotnotes/src/components/Mobile/Breadcrumbs/index.js
@@ -1,56 +1,34 @@
 import React from 'react'
 import './styles.scss'
 import { useLocation } from 'react-router-dom';
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 
-export const Breadcrumbs = props => {
+export const Breadcrumbs = () => {
 
   const { pathname } = useLocation();
   const parsedArray = pathname.split("/");
   const depth = parsedArray.length - 1;
 
-  console.log(parsedArray);
-  
+
   const handleClick = () => {
 
   }
 
-  if(depth === 1){
-    return (
-      <div className="breadcrumb" onClick={() => handleClick()}>
-        <nav>
-          <Link to="/unis">Unis</Link>
-        </nav>
-      </div>
-    )
-  }
-  else if(depth === 2){
-    return (
-      <div className="breadcrumb" onClick={() => handleClick()}>
-        <nav>
-          <Link to="/unis">Unis</Link>
-          <Link to={`/unis/${parsedArray[2]}`}>/{`${parsedArray[2]}`}</Link>
-        </nav>
-      </div>
-    )
 
-  }
-  else{
-    return (
-      <div className="breadcrumb" onClick={() => handleClick()}>
-        <nav>
-          <Link to="/unis">Unis</Link>
-          <Link to={`/unis/${parsedArray[2]}`}>/{`${parsedArray[2]}`}</Link>
-          <Link to={`/unis/${parsedArray[2]}/${parsedArray[3]}`}>/{`${parsedArray[3]}`}</Link>
-        </nav>
-      </div>
-    )
-  }
+  return (
+    <div className="breadcrumb-container" onClick={() => handleClick()}>
 
+      {depth === 1 ? <Link className="active" to="/unis">Unis</Link> : <Link to="/unis">Unis</Link>}
+      {depth === 2 ? <>&nbsp;{">"}&nbsp;<Link className="active" to={`/unis/${parsedArray[2]}`}>{`${parsedArray[2]}`}</Link> </>:
+        depth === 3 ? (<>&nbsp;{">"}&nbsp;<Link to={`/unis/${parsedArray[2]}`}>{`${parsedArray[2]}`}</Link>&nbsp;{">"}&nbsp;
+          <Link className="active" to={`/unis/${parsedArray[2]}/${parsedArray[3]}`}>{`${parsedArray[3]}`}</Link></>) :
+          <></>
+      }
 
-};
-
+    </div >
+  )
+}
 
 export default Breadcrumbs;
