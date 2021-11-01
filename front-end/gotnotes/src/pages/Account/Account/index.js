72820dfd5598550aa9e2a594b87f48358d886e49
KocKaan <kk4291@nyu.edu>
2021-11-01 04:58:02 -0400

    Account Page

--- front-end/gotnotes/src/pages/Account/Account/index.js
+++ front-end/gotnotes/src/pages/Account/Account/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 export const Account = () => {
+    
     return (
         <div>
+            <div>"Your Account"</div>
             <p>Account</p>
+            <nav>
+                <Link to= "/">PictureGrid</Link>
+                <Link to= "/changeProfilePicture">ChangeProfilePicture</Link>
+                <Link to= "/upload">Upload</Link>
+                <Link to= "/notifications">Notifications</Link>
+                <Link to= "/settings">Settings</Link>
+                <Link to= "/deleteAccoutn">DeleteAccount</Link>
+            </nav>
         </div>
+ 
     )
 }
