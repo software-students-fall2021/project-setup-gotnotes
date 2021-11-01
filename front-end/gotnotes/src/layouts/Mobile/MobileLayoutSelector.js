fdba756a03485cf5ce4c05886d64a57c7b268819
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-01 12:05:07 -0400

    some breadcrumbs, some grid layout

--- front-end/gotnotes/src/layouts/Mobile/MobileLayoutSelector.js
+++ front-end/gotnotes/src/layouts/Mobile/MobileLayoutSelector.js
@@ -5,12 +5,15 @@ import { SearchHigherOrder } from './../../components/Mobile/SearchHigherOrder'
 
 import { ListItem } from '../../components/Mobile/ListItem';
 import { GridItem } from '../../components/Mobile/GridItem'
+import { Breadcrumbs } from '../../components/Mobile/Breadcrumbs';
+
 export const MobileLayoutSelector = ({ Component }) => {
 
     const [currentLayout, setCurrentLayout] = useState("list");
 
     return (
         <div className="layout-container">
+            <Breadcrumbs />
             <SearchHigherOrder props={{ currentLayout, setCurrentLayout }} />
             <Component ViewComponent={currentLayout === "list" ? ListItem : GridItem}  />
         </div>
