3ef51d2afd2713fdbe3da9869a09f9b0ef603727
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-01 18:05:44 -0400

    added selects

--- front-end/gotnotes/src/components/Mobile/PageTitle/index.js
+++ front-end/gotnotes/src/components/Mobile/PageTitle/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const PageTitle = ({ props }) => {
+    const { title } = props;
     return (
-        <div>
-            Page Title
+        <div className="page-title-container">
+            <div className="back-button"></div>
+            <div className="page-title">
+                {title}
+            </div>
+            <div className="page-options"></div>
         </div>
     )
 }
--- front-end/gotnotes/src/pages/AddFile/index.js
+++ front-end/gotnotes/src/pages/AddFile/index.js
@@ -3,17 +3,53 @@ import React from 'react'
 //components
 import PageTitle from '../../components/Mobile/PageTitle'
 
+//data
+import { mockUniData, mockClassData } from '../../assets/mocks/mockData'
+
 export const AddFile = ({ props }) => {
     const title = "Add File"
+
+
+    const handleChange = () => {
+
+    }
+
     return (
         <div className="page-container">
-            <PageTitle title={title} />
+            <PageTitle props={{ title }} />
 
             <div className="add-file-container">
                 <div className="file-form-container">
 
+                    <div className="select-container">
+
+                        <label className="select-label">Select University</label>
+
+                        <select className="select" onChange={(e) => handleChange(e.target.value)}>
+
+                            {mockUniData.map(({ itemName }) =>
+                                <option value={itemName}>{itemName}</option>
+                            )}
+
+                        </select>
+                    </div>
+
+                    <div className="select-container">
+
+                        <label className="select-label">Select Class</label>
+
+                        <select className="select" onChange={(e) => handleChange(e.target.value)}>
+
+                            {mockClassData.map(({ itemName }) =>
+                                <option value={itemName}>{itemName}</option>
+                            )}
+
+                        </select>
+                    </div>
+
+
                 </div>
-                <button>Add File</button>
+                <button className="add-file-submit">Add File</button>
 
             </div>
 
