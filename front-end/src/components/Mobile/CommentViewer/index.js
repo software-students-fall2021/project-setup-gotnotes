7d6af81a9a7612deac0e9b75cbba213c63d98399
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-04 20:42:05 -0400

    initial commits to comment viewer, getting mock data from calling component: FileDetails

--- front-end/src/components/Mobile/CommentViewer/index.js
+++ front-end/src/components/Mobile/CommentViewer/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import './styles.scss'
+
+//components
+
+//icons
 
 const CommentViewer = ({ props }) => {
+
+    const { fileComments } = props;
     return (
         <div className="comment-viewer-container">
             Comment Viewer
