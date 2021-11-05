9305bc2336f4a461f40c8d1a827e7e5c37549604
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-04 23:31:37 -0400

    messages showing, will do stylistic changes

--- front-end/src/components/Mobile/Comment/index.js
+++ front-end/src/components/Mobile/Comment/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import './styles.scss'
 
+//components
+import UserDataViewer from '../UserDataViewer';
+
+//icons
+import { LikeIcon } from '../Icons/LikeIcon';
+import { ReplyIcon } from '../Icons/ReplyIcon'
+
 const Comment = ({ props }) => {
 
     const { comment, user, likes, date, replies } = props;
     return (
         <div className="comment-container">
+            <div className="comment-user-data-container">
+                <UserDataViewer props={{ userData: user, shareDate: date, size: "small" }} />
+            </div>
+            <div className="comment-container">
+                <span className="comment">{comment}</span>
+            </div>
+            <div className="comment-interaction-data">
+                <div className="icon-set">
+                    <div className="icon-set">
+                        <LikeIcon props={{ itemID: 1, fontSize: "large" }} />
+                        <span>{likes}</span>
+                    </div>
+                    {replies &&
+                        <div className="icon-set">
+                            <ReplyIcon props={{ itemID: 1, fontSize: "large" }} />
+                            <span>{replies.length}</span>
+                        </div>
+                    }
+                </div>
+            </div>
 
         </div>
     )
