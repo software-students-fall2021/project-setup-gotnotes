9c911469df60be9170be765c9e5add15165d8856
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-06 21:54:01 -0400

    added new skeleton files

--- back-end/Controllers/index.js
+++ back-end/Controllers/index.js
@@ -1,6 +1,7 @@
 var chatController = require('./ChatController');
 var courseController = require('./CourseController');
 var fileController = require('./FileController');
+var filesController = require('./FilesController');
 var uniController = require('./UniController');
 var userController = require('./UserController');
 
@@ -8,6 +9,7 @@ module.exports = {
     chatController,
     courseController,
     fileController,
+    filesController,
     uniController,
     userController
 }
