adf6e6b2a794f08f452a8ae91cb07c59128cf528
jap871 <jap871@nyu.edu>
2021-11-08 22:53:48 -0500

    filename function file services

--- back-end/Services/FileService/index.js
+++ back-end/Services/FileService/index.js
@@ -8,3 +8,13 @@ let fileData = require('./../../Mock/FilesMockData/file.json')
  const get_file_by_filID = function (fileID) {
     return fileData.filter(user => user.fileID === fileID)
 }
+
+
+/**
+ * Get a file by the fileName
+ * @param {*} fileName 
+ * @returns [{userObj}] || []
+ */
+ const get_file_by_filID = function (fileID) {
+    return fileData.filter(user => user.fileID === fileID)
+}
