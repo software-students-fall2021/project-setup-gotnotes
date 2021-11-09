522d99ec06167f2384499a81e251fb14e3bbb126
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-08 19:11:22 -0500

    user service added some functions

--- back-end/Services/UserService/index.js
+++ back-end/Services/UserService/index.js
@@ -1,19 +1,93 @@
-const userData = require('./../../Mock/UsersMockData/users.json')
+let userData = require('./../../Mock/UsersMockData/users.json')
 
-const get_student_by_id = function (user_id) {
-    return userData.filter(user => user.userID === user_id)
+/**
+ * Get a user by the email, which is the unique key of users table
+ * @param {email} email 
+ * @returns [{userObj}] || []
+ */
+const get_user_by_email = function (email) {
+    return userData.filter(user => user.userID === email)
 }
 
-const get_students_by_course_id = function (course_id) {
+/**
+ * Get a user by the username
+ * @param {*} username 
+ * @returns [{userObj}] || []
+ */
+const get_user_by_username = function (username) {
+    return userData.filter(user => user.username === username)
+}
+
+/**
+ * Return the pass hash of a user with given email, if no such user return null
+ * @param {*} email 
+ * @returns String || null
+ */
+const get_user_pass_hash_by_email = function (email) {
+    const user = userData.filter(user => user.userID === email);
+    if (user[0].passwordHash)
+        return user[0].passwordHash
+    return null;
+}
+
+/**
+ * Checks whether user with given email is an admin, returns true if admin, false if not
+ * @param {*} email 
+ * @returns boolean
+ */
+const get_user_authority_by_email = function (email) {
+    return get_user_by_email(email)[0].isAdmin;
+}
+
+/**
+ * Return the avatar url of the user with given email
+ * @param {*} email 
+ * @returns String || null
+ */
+const get_user_avatar_url_by_email = function (email) {
+    const user = userData.filter(user => user.userID === email);
+    if (user[0].userAvatarUrl)
+        return user[0].userAvatarUrl
+    return null;
+}
+
+/**
+ * sets the new user avatar url for the user with given email, returns 0 if success, 1 if no such user
+ * @param {*} email 
+ * @param {*} newAvatarUrl 
+ * @returns 1 || 0
+ */
+const set_user_avatar_url_by_email = function (email, newAvatarUrl) {
+    const currentUser = userData.filter(user => user.userID === email);
+
+    if(!currentUser) return 1;
+
+    userData = userData.filter(user => user.userID !== email);
+
+    currentUser.userAvatarUrl = newAvatarUrl;
+
+    userData.push(currentUser);
+
+    return 0;
+
+}
+
+
+
+
+
+
+const get_users_by_course_id = function (course_id) {
     const students_by_course = [];
     userData.map((user) => {
         let count = 0;
         user.userSubscribed.map((course) => course.courseID === course_id && count++)
-        count > 0 && students_by_course.push({username: user.username, userSubscribed: user.userSubscribed})
+        count > 0 && students_by_course.push({ username: user.username, userSubscribed: user.userSubscribed })
     })
 
     return students_by_course;
 }
+
 
 
 
