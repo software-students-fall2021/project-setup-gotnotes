04e9a56d973be5a68d10a80c65bc14bef4cf9a22
KocKaan <kk4291@nyu.edu>
2021-11-09 18:47:33 -0500

    Added course enroleld student and course files

--- back-end/Services/CourseService/index.js
+++ back-end/Services/CourseService/index.js
@@ -1,4 +1,5 @@
 let courseData = require('./../../Mock/CoursesMockData/courses.json')
+//Although we can get by id or course name the primary keys are courseID
 
 /**
  * Get course by ID
@@ -14,6 +15,33 @@ let courseData = require('./../../Mock/CoursesMockData/courses.json')
  * @param {*} courseName 
  * @returns [{userObj}] || []
  */
- exports.get_uni_name = function (courseName) {
+ exports.get_course_name = function (courseName) {
     return uniData.filter(course => course.courseName === courseName)
 }
+
+
+/**
+ * Get course enrolled students
+ * @param {*} courseID
+ * @returns [{courseEnroleldStudentsObj}] || []
+ */
+ exports.get_course_enrolled  = function (courseID) {
+    const course = get_course_id(courseID)[0];
+    if (course?.courseEnrolledStudents)
+        return user.courseEnrolledStudents;
+    return null;
+}
+
+/**
+ * Get course shared files
+ * @param {*} courseID 
+ * @returns [{courseEnroleldStudentsObj}] || []
+ */
+ exports.get_course_files = function (courseID) {
+    const course = get_course_id(courseID)[0];
+    if (course?.courseSharedFiles)
+        return course.courseSharedFiles;
+    return null;
+}
+
+
