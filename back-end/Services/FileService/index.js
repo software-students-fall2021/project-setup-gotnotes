0ee2cbc6f097db651cc530cc68c91fbaf0ddefb0
jap871 <jap871@nyu.edu>
2021-11-08 23:01:28 -0500

    get_user_liked function added to file services

--- back-end/Services/FileService/index.js
+++ back-end/Services/FileService/index.js
@@ -1,12 +1,13 @@
 let fileData = require('./../../Mock/FilesMockData/file.json')
+import { get_user } from ('./../UserService/index.json')
 
 /**
  * Get a file by the fileID
  * @param {*} fileID 
  * @returns [{userObj}] || []
  */
- const get_file_by_filID = function (fileID) {
-    return fileData.filter(user => user.fileID === fileID)
+ const get_file_by_fileID = function (fileID) {
+    return fileData.filter(file => file.fileID === fileID)
 }
 
 
@@ -15,6 +16,13 @@ let fileData = require('./../../Mock/FilesMockData/file.json')
  * @param {*} fileName 
  * @returns [{userObj}] || []
  */
- const get_file_by_filID = function (fileID) {
-    return fileData.filter(user => user.fileID === fileID)
+ const get_file_by_filName = function (fileName) {
+    return fileData.filter(file => file.fileName === fileName)
+}
+
+exports.get_user_liked = function (email) {
+    const user = get_user(email)[0];
+    if (user?.userLiked)
+        return user.userLiked
+    return null;
 }
