c717c24b0305fca774c838103fbb0c1cc66bf4ca
KocKaan <kk4291@nyu.edu>
2021-11-09 19:19:32 -0500

    added setting course name and adding new student enrolleed

--- back-end/Services/CourseService/index.js
+++ back-end/Services/CourseService/index.js
@@ -45,3 +45,49 @@ let courseData = require('./../../Mock/CoursesMockData/courses.json')
 }
 
 
+/**
+ * Set course name- it might be updated
+ * @param {*} courseID 
+ * @param {*} courseName 
+ * @returns 1 if succesfule edit 0 doesnt happen
+ */
+exports.set_course_courseName  = function (courseID, courseName) {
+    const course = get_course_id(courseID)[0];
+
+    if (!course) return 0;
+
+    courseData = courseData.filter(course => course.courseID !== courseID);
+
+    currentUser[0].courseName= courseName;
+
+    courseData.push(couse);
+
+    return 1;
+
+}
+
+/**
+ * add new student to the course list
+ * @param {*} courseID 
+ * @param {*} studentID  
+ * @returns 1 if succesfule addition 0 doesnt happen
+ */
+ exports.add_course_student = function (courseID, userID) {
+    const course = get_course_id(courseID)[0];
+
+    if (!course) return 0;
+
+    courseData = courseData.filter(course => course.courseID !== courseID);
+
+    const addStudent= {userID: userID }
+
+    course[0].courseEnrolledStudents.push(addStudent);
+
+    courseData.push(course);
+
+    return 1;
+
+}
+
+
+
