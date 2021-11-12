f9db50591e016c8641793f8e5238c68708ec9cdc
m4rker11 <79331578+m4rker11@users.noreply.github.com>
2021-11-12 16:55:49 -0500

    Mongoose and atlas were added
    I made us an atlas so from here we populate it with calls to db

--- back-end/server.js
+++ back-end/server.js
@@ -22,15 +22,14 @@ const {
 const app = express()
 
 
-/**
- * //Set up mongoose connection
- * var mongoose = require('mongoose');
- * var mongoDB = process.env.DB_URL;
- * mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
- * var db = mongoose.connection;
- * db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
- * 
- */
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = process.env.DB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+ 
 
 
 app.use(cors())
