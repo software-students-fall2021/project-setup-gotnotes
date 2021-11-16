b532f6fab86b0dc5dfac0a5f77e6e4bf47d65198
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-16 18:35:55 -0500

    unis are getting from the backend now

--- front-end/src/pages/Search/Unis/index.js
+++ front-end/src/pages/Search/Unis/index.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './styles.scss'
 
 //mock data
-import { mockUniData } from '../../../assets/mocks/mockData'
+//import { mockUniData } from '../../../assets/mocks/mockData'
 
 export const Unis = ({ ViewComponent, activeClass }) => {
+
+    const [uniData, setUniData] = useState(null);
+
+    useEffect(async () => {
+        const result = await axios(
+            'http://localhost:4000/unis',
+        );
+        console.log(result.data)
+        setUniData(result.data);
+    }, [])
 
     return (
         <div className={activeClass === "grid" ? "unis grid" : "unis"}>
 
-            {mockUniData.map(({ itemID, itemName, itemLogoPath, itemType, courseCount }) => (
+            {uniData && uniData.map(({ uniID: itemID, uniName: itemName, uniLogoPath: itemLogoPath, uniStudentCount: courseCount }) => (
                 <ViewComponent
                     key={itemID}
-                    props={{ itemID, itemName, itemLogoPath, itemType, courseCount }}
+                    props={{ itemID, itemName, itemLogoPath, itemType: "uni", courseCount }}
                 />
 
             ))}
