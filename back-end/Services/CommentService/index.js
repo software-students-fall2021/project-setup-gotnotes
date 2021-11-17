1d0be177f06357dfa0617d1ce962c9463b089f75
m4rker11 <79331578+m4rker11@users.noreply.github.com>
2021-11-17 12:42:33 -0500

    Comment service completed
    Will crank out the rest of the services, can someone work on the controllers? I will see how it is supposed to be done and join forces

--- back-end/Services/CommentService/index.js
+++ back-end/Services/CommentService/index.js
@@ -1,5 +1,71 @@
 const commentData = require('./../../Mock/CommentsMockData/comments.json');
+import comment from '../../Models/Comment/index'
 exports.commentData = commentData;
+exports.count_comment = (comment) => {
+    return comment.countDocuments({});
+}
+
+exports.make_comment = (Comment, commentedBy, parentCommentID) => {
+    if (parentCommentID != null && parentCommentID != undefined && parentCommentID != '' && parentCommentID != 0) {
+        let newComment = new comment({
+            commentID: count_comment(comment) + 1,
+            comment: Comment,
+            commentedBy: commentedBy,
+            parentCommentID: parentCommentID,
+            commentedAt: new Date()
+        })
+    }
+    else {
+        let newComment = new comment({
+            commentID: count_comment(comment) + 1,
+            comment: Comment,
+            commentedBy: commentedBy,
+            commentedAt: new Date()
+        })
+    }
+    newComment.save(err => {
+        if (err) {
+            console.log(err);
+        }
+    })
+}
+
+exports.like_comment = (commentID, likedBy) => {
+    comment.findOne({ commentID: commentID }, (err, comment) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            comment.likedBy.push(likedBy);
+            comment.save(err => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
+    })
+}
+exports.dislike_comment = (commentID, dislikedBy) => {
+    comment.findOne({ commentID: commentID }, (err, comment) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            comment.dislikedBy.push(dislikedBy);
+            comment.save(err => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
+    })
+}
+
+exports.get_comment = (commentID) => {
+    return comment.findOne({ commentID: commentID });
+}
+
+
 
 
 /**
