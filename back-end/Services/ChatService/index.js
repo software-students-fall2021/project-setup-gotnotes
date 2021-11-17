7dc6159de8ae5795632ba4ae134571e81397b911
m4rker11 <79331578+m4rker11@users.noreply.github.com>
2021-11-17 12:20:50 -0500

    Chat service now supports getting and pushing data to and from atlas
    Have not attempted actually calling these functions from the server.js as not sure how to pass the mongoConnection to it

--- back-end/Services/ChatService/index.js
+++ back-end/Services/ChatService/index.js
@@ -1,93 +1,216 @@
 const { use } = require('chai');
 let chatData = require('./../../Mock/ChatsMockData/chats.json')
-
+var mongoose = require("mongoose");
+var axios = require("axios");
+var db = mongoose.connection;
+import chat from '../../Models/Chat/index'
 // Comment By Mark Zarutin
-// add chat function
-// get chat by chatID
-
-
-//not addind addUni by university name or any specific key. If a university is added it should have
-//everything containing uniID, uniName, uniLogoPath but it doesnt need uniStudent[] and UniCourses[]
-// as there might not be any student registered. 
-
-
 /**
- * Get chat by chatID
- * @param {chatID} chatID
- * @returns [{uniObj}] || []
+ * Get chat by courseID and chatName
+ * @param {courseID} courseID
+ * @param {chatName} chatName
  */
- exports.get_chat = function (chatID) {
-    return chatData.filter(chat => chat.chatID === chatID);
+
+exports.addChat = (courseID, chatName) => {
+    let chat_instance = new chat({
+        courseID: courseID,
+        chatName: chatName,
+        chatMembers: [],
+        chatContent: []
+    })
+    chat_instance.save(function (err) {
+        if (err) {
+            console.log(err);
+        }
+    })
 }
 /**
- * Adds a new uni to the database 
- * @param {*} uniID is the primary key
- * @param {*} uniName is the name of the university 
- * @param {*} uniLogPath is the path to the logo of the uni
- * @returns 0 if success, 1 of no such user
+ * Adds message by courseID and chatName
+ * @param {courseID} courseID
+ * @param {chatName} chatName
+ * @param {message} message
+ * @param {messageBy} messageBy
  */
-exports.create_chat = function (uniID, chatName) {
-     //its not gone be like this when
-
-     var jsonFile= chatData;
-     var obj= JSON.parse(JSON.stringify(jsonFile));
-     let newChat = {
-        "chatID": obj.length + 1,
-        "uniID": uniID,
-        "chatName": chatName,
-        "chatMembers": [],
-        "chatContent": [],
-      };
-
-      obj.push(newChat);
-      jsonStr= JSON.stringify(obj);
-      return 1; 
+exports.addMessage = (courseID, chatName, message, messageBy) => {
+    chat.findOne({ courseID: courseID, chatName: chatName }, function (err, chat) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            chat.chatContent.push({
+                message: message,
+                messageBy: messageBy,
+                messageDate: new Date(),
+                messageLikedBy: []
+            })
+            chat.save(function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
+    })
 }
-
-exports.add_student_to_chat = function (chatID, userID, userName) {
-    let chat = chatData.filter(chat => chat.chatID === chatID);
-    chat[0].chatMembers.push({userID: userID, userName: userName});
-    chatData.push(chat);
-    // console.log(chat[0].chatMembers.filter(member => member.userID === userID));
-}
-// add_student_to_chat(1, 34, "Mark");
-// console.log(chatData[0].chatMembers.filter(member => member.userName === "Mark"));
-
 /**
- * Get uni by its university name
- * @param {uniID} uniID 
- * @returns [{uniObj}] || []
+ * Likes message by courseID, chatName, userID, and message
+ * @param {courseID} courseID
+ * @param {chatName} chatName
+ * @param {message} message
+ * @param {userID} userID
  */
- exports.get_chat_chatName = function (chaID) {
-     const chat= get_chat(chatID);
-     if(chat?.chatName){
-         return chat.chatName;
-     }
-     return null;
+exports.like_message = (courseID, chatName, message, userID) => {
+    chat.findOne({ courseID: courseID, chatName: chatName }, function (err, chat) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            chat.chatContent.findOne({ message: message }, function (err, content) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    content.messageLikedBy.push(userID)
+                    chat.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
+/**
+ * Gets all chats for a courseID
+ * @param {courseID} courseID
+ * @returns {chat} || null
+ */
+exports.get_chatName = (courseID) => {
+    chat.find({ courseID: courseID }, function (err, chat) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            chatNames = []
+            for (let i = 0; i < chat.length; i++) {
+                chatNames.push(chat[i].chatName)
+            }
+            return chatNames
+        }
+    })
+}
+/**
+ * Gets all chat users for a courseID and chatName
+ * @param {courseID} courseID
+ * @param {chatName} chatName
+ * @returns {chat.chatMembers} || null
+ */
+exports.get_Chat_Users = (courseID, chatName) => {
+    chat.findOne({ courseID: courseID, chatName: chatName }, function (err, chat) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            return chat.chatMembers
+        }
+    })
+}
+/**
+ * gets the entire chat object for a courseID and chatName
+ * @param {courseID} courseID
+ * @param {chatName} chatName
+ * @returns {chat} || null
+ */
+exports.get_chat = (courseID, chatName) => {
+    chat.findOne({ courseID: courseID, chatName: chatName }, function (err, chat) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            return chat
+        }
+    })
 }
 
-exports.add_message_to_chat = function (chatID, message, userID, userName) {
-    let addition = {
-        "message":message,
-        "messageBy":userID,
-        "messageDate": new Date(),
-        "messageLikedBy":[]
-    }
-    let chat = chatData.filter(chat => chat.chatID === chatID);
-    if(!chat){
-        return null;
-    }
-    chat[0].chatContent.push(addition);
-    chatData.push(chat);
-    return 1;
-}
-exports.like_message = function (chatID, message, userID) {
-    let like = { "userID": userID };
-    let chat = chatData.filter(chat => chat.chatID === chatID);
-    if(!chat){
-        return null;
-    }
-    chat[0].chatContent.filter(message => message.message === message).messageLikedBy.push(like);
-    chatData.push(chat);
-    return 1;
-}
+
+
+// THESE ARE THE NON-DB FUNCTIONS
+// /**
+//  * Get chat by chatID
+//  * @param {chatID} chatID
+//  * @returns [{uniObj}] || []
+//  */
+//  exports.get_chat = function (chatID) {
+//     return chatData.filter(chat => chat.chatID === chatID);
+// }
+// /**
+//  * Adds a new uni to the database 
+//  * @param {*} chatID is the primary key
+//  * @param {*} uniID is the id of the university 
+//  * @returns 0 if success, 1 of no such user
+//  */
+// exports.create_chat = function (uniID, chatName) {
+//      //its not gone be like this when
+
+//      var jsonFile= chatData;
+//      var obj= JSON.parse(JSON.stringify(jsonFile));
+//      let newChat = {
+//         "chatID": obj.length + 1,
+//         "uniID": uniID,
+//         "chatName": chatName,
+//         "chatMembers": [],
+//         "chatContent": [],
+//       };
+
+//       obj.push(newChat);
+//       jsonStr= JSON.stringify(obj);
+//       return 1; 
+// }
+
+// exports.add_student_to_chat = function (chatID, userID, userName) {
+//     let chat = chatData.filter(chat => chat.chatID === chatID);
+//     chat[0].chatMembers.push({userID: userID, userName: userName});
+//     chatData.push(chat);
+//     // console.log(chat[0].chatMembers.filter(member => member.userID === userID));
+// }
+// // add_student_to_chat(1, 34, "Mark");
+// // console.log(chatData[0].chatMembers.filter(member => member.userName === "Mark"));
+
+// /**
+//  * Get uni by its university name
+//  * @param {chatID} chatID 
+//  * @returns [{uniObj}] || []
+//  */
+//  exports.get_chat_chatName = function (chaID) {
+//      const chat= get_chat(chatID);
+//      if(chat?.chatName){
+//          return chat.chatName;
+//      }
+//      return null;
+// }
+
+// exports.add_message_to_chat = function (chatID, message, userID, userName) {
+//     let addition = {
+//         "message":message,
+//         "messageBy":userID,
+//         "messageDate": new Date(),
+//         "messageLikedBy":[]
+//     }
+//     let chat = chatData.filter(chat => chat.chatID === chatID);
+//     if(!chat){
+//         return null;
+//     }
+//     chat[0].chatContent.push(addition);
+//     chatData.push(chat);
+//     return 1;
+// }
+// exports.like_message = function (chatID, message, userID) {
+//     let like = { "userID": userID };
+//     let chat = chatData.filter(chat => chat.chatID === chatID);
+//     if(!chat){
+//         return null;
+//     }
+//     chat[0].chatContent.filter(message => message.message === message).messageLikedBy.push(like);
+//     chatData.push(chat);
+//     return 1;
+// }
