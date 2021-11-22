a87e88f3a7b1a305da6013e9baf565b1fc958c41
jap871 <jap871@nyu.edu>
2021-11-21 21:41:13 -0500

    Co-authored-by: HuseyinAtacanDemir <HuseyinAtacanDemir@users.noreply.github.com>

--- front-end/src/pages/Account/Account/index.js
+++ front-end/src/pages/Account/Account/index.js
@@ -5,21 +5,32 @@ import UserDataViewer from "../../../components/Mobile/UserDataViewer";
 import "./styles.scss";
 import { DonutLarge } from "@mui/icons-material";
 
+const editUserData = () => {
+  console.log("submit the edits to the server");
+  return true;
+};
+
 export const Account = ({ props }) => {
   const userID = "cdies0@netlog.com";
 
-  const [isEditActive, setIsEditActive] = useState(false);
+  const [isEditActive, setIsEditActive] = useState(null);
   const [userData, setUserData] = useState(null);
 
-  useEffect( () => {
+  const [userInputState, setUserInputState] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  useEffect(() => {
     //post request with userID: userID to http://localhost/4000/account
     axios("http://localhost:4000/account", {
       method: "POST",
       data: {
-        userID: userID
-      }
-    }).then(res => setUserData(res.data[0]))
-    .catch(err => console.log(err))
+        userID: userID,
+      },
+    })
+      .then((res) => setUserData(res.data[0]))
+      .catch((err) => console.log(err));
 
     //console.log(result);
     //setUserData(result.data[0]);
@@ -27,19 +38,76 @@ export const Account = ({ props }) => {
     //setUserData(result.data)
   }, []);
 
-  
-
   return userData ? (
     <div>
       Account Page
-      {isEditActive && <div className="random"></div>}
-      {!isEditActive && (
-        <div className="accountList">
-          <UserAvatar props={{ userAvatarUrl: userData.userAvatarUrl, size: "large", editActive: isEditActive }} />
-          <UserDataViewer props={{ userData: userID, avatarSize: "large" }} />
+      {isEditActive && (
+        <div className="edit-page">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              editUserData(userInputState);
+              setUserInputState({
+                firstName: "",
+                lastName: "",
+              });
+            }}
+          >
+            <input
+              type="text"
+              placeholder={userData.firstName}
+              value={userInputState.firstName}
+              onChange={(e) =>
+                setUserInputState({
+                  ...userInputState,
+                  firstName: e.target.value,
+                })
+              }
+            />
 
-          {userID}
-        </div>)}
+            <input type="submit" value="Submit Edits" />
+          </form>
+
+          <div classname="submit-button">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                editUserData(userInputState);
+                setIsEditActive(false);
+              }}>
+              <input type="submit" value="Done" />
+            </form>
+          </div>
+        </div>
+      )}
+      {!isEditActive && (
+        <div className="account-list">
+          <div className="user-avatar-component-container">
+            <UserAvatar
+              props={{
+                userAvatarUrl: userData.userAvatarUrl,
+                size: "large",
+                editActive: isEditActive,
+              }}
+            />
+          </div>
+
+          <div className="user-data-viewer-container">
+            <UserDataViewer props={{ userData: userID, avatarSize: "large" }} />
+          </div>
+
+          <div classname="submit-button">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                editUserData(userInputState);
+                setIsEditActive(true);
+              }}>
+              <input type="submit" value="Edit" />
+            </form>
+          </div>
+        </div>
+      )}
       {/*
             You might wanna have a look at the UserAvatar component that I wrote a while back
             skeleton of account page:
