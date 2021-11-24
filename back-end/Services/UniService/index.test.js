54bf0ecaf9fbe3f7fa278976b51478aebf3528f5
KocKaan <kk4291@nyu.edu>
2021-11-23 21:54:32 -0500

    Test refactoring for the database

--- back-end/Services/UniService/index.test.js
+++ back-end/Services/UniService/index.test.js
@@ -3,18 +3,19 @@ const chai = require('chai');
 const { expect } = chai;
 chai.use(deepEqualInAnyOrder);
 
-var uniService = require('./index')
-let uniData = require('./../../Mock/UnisMockData/unis.json')
+var UniService = require('./index')
+let uniData = UniService.uniData
 const testUni = uniData[0];
 
 describe('uniService', function () {
     describe('get_uni()', function () {
         it('should return arr with one uni obj in it given valid uniID', function () {
 
-            const uni = uniService.get_uni(testUni.uniID)
+            const uni = UniService.get_uni(testUni.uniID)
 
             expect(uni[0]).to.deep.equalInAnyOrder(testUni);
 
         });
     });
 });
+
