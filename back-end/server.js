055b3f4fe58a61a91ce1bf73c8eb2512b8084a86
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-28 00:56:18 -0500

    mongodb mongoose connection refactored, connection bug due to multiple mongoose.connect function calls is callsfixed

--- back-end/server.js
+++ back-end/server.js
@@ -1,28 +1,19 @@
 require("dotenv").config();
+const PORT = process.env.PORT;
 
 const express = require("express");
 const fileUpload = require("express-fileupload");
-
 const cors = require("cors");
-
-const path = require("path");
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-
-const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const { accountRouter, chatRouter, searchRouter } = require("./Routes");
+const { db_connect } = require("./Services/Database");
 
 const app = express();
-
-//Set up mongoose connection
-var mongoose = require("mongoose");
-var mongoDB = process.env.DB_URL;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+const db = db_connect();
 
 app.use(cors());
 app.use(fileUpload());
@@ -33,6 +24,7 @@ app.use("/unis", searchRouter.unisRouter);
 app.use("/unis/:uni", searchRouter.coursesRouter);
 app.use("/unis/:uni/:course", searchRouter.filesRouter);
 app.use("/unis/:uni/:course/:file", searchRouter.fileRouter);
+
 app.use("/chats", chatRouter);
 app.use("/account", accountRouter);
 
@@ -41,57 +33,59 @@ app.get("/admin", (req, res) => {
   res.send("Admin Request Received");
 });
 
+/* 
+
 app.post("/admin", (req, res) => {});
 
-// app.get('/signup', (req, res) => {
-//     // let token = jwt.sign(
-//     //     { count: 0 },
-//     //     process.env.JWT_SECRET,
-//     //     { expiresIn: '5m'}
-//     // )
-//     // <input type='hidden' name='jwt' value='${token}'/>
-//     res.send(`<html><body><form action='http://localhost:${PORT}/signup' method='post' ><label for='email'>Email</label><input type='email' name='email'/><label for='password'>Password</label><input type='password' name='password' /><input type='submit' value='Login' /></form></body></html>`)
-// })
+app.get('/signup', (req, res) => {
+    let token = jwt.sign(
+        { count: 0 },
+        process.env.JWT_SECRET,
+        { expiresIn: '5m'}
+    )
+    <input type='hidden' name='jwt' value='${token}'/>
+    res.send(`<html><body><form action='http://localhost:${PORT}/signup' method='post' ><label for='email'>Email</label><input type='email' name='email'/><label for='password'>Password</label><input type='password' name='password' /><input type='submit' value='Login' /></form></body></html>`)
+})
 
-// app.post('/signup', async (req, res) => {
-//     if(req.body.email && req.body.password){
-//         try {
-//             //const err = Validate(email, password);
-//             //console.log("validation err: ", err)
-//             //if (err) throw new Error(err)
+app.post('/signup', async (req, res) => {
+    if(req.body.email && req.body.password){
+        try {
+            const err = Validate(email, password);
+            console.log("validation err: ", err)
+            if (err) throw new Error(err)
 
-//             const email = req.body.email;
-//             const password = req.body.password;
+            const email = req.body.email;
+            const password = req.body.password;
 
-//             const newUser = await db('users').insert({ email: email, pass: await bcrypt.hash(password, 10) })
+            const newUser = await db('users').insert({ email: email, pass: await bcrypt.hash(password, 10) })
 
-//             const token = jwt.sign(
-//                 { id: newUser.id, email: newUser.email },
-//                 process.env.JWT_SECRET,
-//                 { expiresIn: '30m' }
-//             )
+            const token = jwt.sign(
+                { id: newUser.id, email: newUser.email },
+                process.env.JWT_SECRET,
+                { expiresIn: '30m' }
+            )
 
-//                 res.json([{token: `Bearer ${token}`}])
+                res.json([{token: `Bearer ${token}`}])
 
-//         } catch (error) {
-//             if (error.message.includes("Duplicate entry")) {
-//                 error.message = "A user with that email already exists"
-//             }
-//             res.json([{error: error.message}])
-//         }
+        } catch (error) {
+            if (error.message.includes("Duplicate entry")) {
+                error.message = "A user with that email already exists"
+            }
+            res.json([{error: error.message}])
+        }
 
-//     }else{
-//         res.json([{error: 'please enter all fields'}])
-//     }
-// })
+    }else{
+        res.json([{error: 'please enter all fields'}])
+    }
+})
 
 app.get("/login", (req, res) => {
-  // let token = jwt.sign(
-  //     { count: 0 },
-  //     process.env.JWT_SECRET,
-  //     { expiresIn: '5m'}
-  // )
-  // <input type='hidden' name='jwt' value='${token}'/>
+  let token = jwt.sign(
+      { count: 0 },
+      process.env.JWT_SECRET,
+      { expiresIn: '5m'}
+  )
+  <input type='hidden' name='jwt' value='${token}'/>
   res.send(
     `<html><body><form action='http://localhost:${PORT}/login' method='post' ><label for='email'>Email</label><input type='email' name='email'/><label for='password'>Password</label><input type='password' name='password' /><input type='submit' value='Login' /></form></body></html>`
   );
@@ -132,7 +126,7 @@ app.post("/login", async (req, res) => {
     },
   ]);
 });
-
+ */
 app.listen(PORT, () => {
   console.log(`Server ready at ${process.env.PUBLIC_URL}:${PORT}`);
 });
