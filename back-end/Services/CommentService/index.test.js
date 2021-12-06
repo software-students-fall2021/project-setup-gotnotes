9d5654b283d13fbe3164a0438e7b252b67250b74
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-12-06 17:06:26 -0500

    commented out tests

--- back-end/Services/CommentService/index.test.js
+++ back-end/Services/CommentService/index.test.js
@@ -3,9 +3,9 @@ const chai = require("chai");
 const { expect } = chai;
 chai.use(deepEqualInAnyOrder);
 
-var CommentService = require("./index");
-let commentData = CommentService.commentData;
-const testComment = commentData[0];
+// var CommentService = require("./index");
+// let commentData = CommentService.commentData;
+// const testComment = commentData[0];
 
 // describe("uniService", function () {
 //   describe("get_comment()", function () {
