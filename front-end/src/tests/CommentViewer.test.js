c53fc1756705a6f773988ea81d5f7e627cfbc22f
KocKaan <kk4291@nyu.edu>
2021-12-08 18:53:28 -0500

    CommentViewer render test

---
+++ front-end/src/tests/CommentViewer.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { expect, assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import CommentViewer from './src/components/Mobile/CommentViewer/index';
+
+
+describe('CommentViewer', () => {
+
+    it('should render CommentViewer', () => {
+
+        const wrapper = shallow(<CommentViewer />);
+
+        expect(wrapper.containsAllMatchingElements([
+
+            <CommentViewer />,
+
+        ])).to.equal(true);
+    });
+
+    it('should exists', () => {
+
+        assert.isDefined(CommentViewer)
+    })
+});
