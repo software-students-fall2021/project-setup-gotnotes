3bf1fbe469a323aa02bc1bcf4b6029a03a84ca3e
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-12-08 13:51:55 -0500

    srever side fucntions for socket io complete

--- back-end/webSocketServer.js
+++ back-end/webSocketServer.js
@@ -1,47 +1,123 @@
+const MessageService = require("./Services/ChatService");
+
 const io = require("socket.io")(80, {
-    cors: {
-      origin: "http://localhost:3000",
-    },
-  });
-  
-  let users = [];
-  
-  const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-      users.push({ userId, socketId });
-  };
-  
-  const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
-  };
-  
-  const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
-  };
-  
-  io.on("connection", (socket) => {
-    //when ceonnect
-    console.log("a user connected.");
-  
-    //take userId and socketId from user
-    socket.on("addUser", (userId) => {
-      addUser(userId, socket.id);
-      io.emit("getUsers", users);
-    });
-  
-    //send and get message
-    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-      const user = getUser(receiverId);
-      io.to(user.socketId).emit("getMessage", {
-        senderId,
-        text,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+let rooms = [];
+
+let chats = [];
+
+const addUser = (courseId, userId, username, userAvatarUrl, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ courseId, userId, username, userAvatarUrl, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUsers = (courseId) => {
+  return users.filter((user) => user.courseId === courseId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+const getUserBySocketId = (socketId) => {
+  return users.find((user) => user.socketId == socketId);
+};
+
+const addChat = (courseId) => {
+  !chats.some((chat) => chat.courseId == courseID) &&
+    chats.push({ courseId: courseId, content: [] });
+};
+
+const addChatContent = (courseId, messageObj) => {
+  const currentChat = chats.find((chat) => chat.courseId == courseId);
+  currentChat.content.push(messageObj);
+};
+
+const editChatContent = (courseId, messageObj, likeUserId) => {
+  const currentChat = chats.find((chat) => chat.courseId == courseId);
+  const currentMessage = currentChat.content.find(
+    (message) =>
+      message.dateSent.toString() == messageObj.dateSent.toString() &&
+      message.sender.toString() == messageObj.sender.toString()
+  );
+  if (
+    !currentMessage.likes.some(
+      (userId) => userId.toString() == likeUserId.toString()
+    )
+  ) {
+    currentMessage.likes.push(likeUserId.toString());
+  } else {
+    currentMessage.likes = currentMessage.likes.filter(
+      (userId) => userId.toString() != likeUserId.toString()
+    );
+  }
+  return currentMessage;
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //
+  socket.on(
+    "JoinCourseChat",
+    ({ courseId, userId, username, userAvatarUrl }) => {
+      socket.join(courseId);
+      addUser(courseId, userId, username, userAvatarUrl, socket.id);
+      addChat(courseId);
+      io.in(courseId).emit("users", getUsers(courseId));
+    }
+  );
+
+  //send and get message
+  socket.on(
+    "SendMessage",
+    ({ courseId, userId, message, dateSent = Date.now(), likes = [] }) => {
+      addChatContent(courseId, {
+        message: message,
+        sender: userId,
+        dateSend: dateSent,
+        likes: [],
       });
-    });
-  
-    //when disconnect
-    socket.on("disconnect", () => {
-      console.log("a user disconnected!");
-      removeUser(socket.id);
-      io.emit("getUsers", users);
-    });
+      io.in(courseId).emit("getMessage", {
+        sender: userId,
+        message,
+        dateSent,
+        likes,
+      });
+    }
+  );
+
+  //send and get like data
+  socket.on(
+    "LikeMessage",
+    ({ courseId, userId, message, dateSent, likeUserId }) => {
+      const currentMessage = editChatContent(
+        courseId,
+        { sender: userId, message: message, dateSent: dateSent },
+        likeUserId
+      );
+      io.in(courseId).emit("getLike", {
+        currentMessage,
+      });
+    }
+  );
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    const user = getUserBySocketId(socket.id);
+    removeUser(socket.id);
+    io.in().emit("getUsers", getUsers(user.courseId));
   });
+});
