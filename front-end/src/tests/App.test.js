35fabfc3f222784acb3495119861d13bc370cb24
Matthew Apuya <m.apuya19@gmail.com>
2021-12-09 18:33:20 -0500

    attempt to fix app.test.js

--- front-end/src/tests/App.test.js
+++ front-end/src/tests/App.test.js
@@ -1,27 +1,17 @@
 import React from "react";
-import { configure } from 'enzyme';
 import { configure, shallow } from "enzyme";
 import chai, { expect } from "chai";
 import chaiEnzyme from "chai-enzyme";
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "../App.js";
 
 configure({ adapter: new Adapter() });
 
-//could be run using mocha App.test.js through command line
-
-configure({
-  adapter: new Adapter(),
-});
 describe("Testin <App/> Component", () => {
-  it("App renders a message", () => {
+  it("App renders correctly", () => {
     const wrapper = shallow(<App />);
-    const message = (
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    );
-    expect(wrapper).to.contain(message);
+      
+    expect(wrapper.contains(<Switch />)).to.equal(true);
   });
   chai.use(chaiEnzyme());
 });
