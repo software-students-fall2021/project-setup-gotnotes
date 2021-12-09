8ba370cb2aae5223dbbcb6f2198201efa57a5cc6
KocKaan <kk4291@nyu.edu>
2021-12-08 19:16:11 -0500

    MessageInput render test

---
+++ front-end/src/tests/MessageInput.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { expect, assert } from 'chai';
+import { shallow, mount } from 'enzyme';
+import MessageInput from './src/components/Mobile/MessageInput/index';
+
+
+describe('MessageInput', () => {
+
+    it('should render MessageInput', () => {
+
+        const wrapper = shallow(<MessageInput />);
+
+        expect(wrapper.containsAllMatchingElements([
+
+            <MessageInput />,
+
+        ])).to.equal(true);
+    });
+
+    it('should exists', () => {
+
+        assert.isDefined(MessageInput)
+    })
+});
